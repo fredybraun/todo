42d406251a44c7646e2592dd2a45b99f7aeffd0c
Fredy Braun <fredy.braun@gmail.com>
2022-11-23 20:49:40 -0300

    create component status check

--- web/src/components/Tasks.tsx
+++ web/src/components/Tasks.tsx
@@ -1,3 +1,4 @@
+import { StatusCheck } from './StatusCheck';
 
 export interface TaskProps {
     taskList: [{
@@ -12,18 +13,21 @@ export function TaskList(props: TaskProps) {
     const tasks = props.taskList;
     const listTasks = tasks.map((tasks) =>
     <a  href={tasks.id}>
-    <li className='
-      h-16 w-542 mt-1
-      bg-blue-deep 
-      border-solid 
-      border-2 
-      border-gray-700 
-      rounded-md 
-      ' key={tasks.id}>
-       <input type='checkbox' className='h-4 w-4'  />
-        {tasks.name}
-    </li>
-  </a>
+        <li className='
+            flex
+            h-16 w-542 mt-1
+            bg-blue-deep 
+            border-solid 
+            border-2 
+            border-gray-700 
+            rounded-md 
+            ' key={tasks.id}>
+
+            <StatusCheck status={tasks.status}/>
+
+            <div className='ml-6 mt-4 text-white'>{tasks.name}</div>
+        </li>
+    </a>
     );
     return (
       <ul>{listTasks}</ul>
